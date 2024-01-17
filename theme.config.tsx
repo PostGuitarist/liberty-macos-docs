7bd43bc7b152ab6444e563bdeb6f922a42adb989
Zaden Connell <zaden.connell@gmail.com>
2024-01-17 15:21:54 -0500

    reinit use Next

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>LU CS Docs</span>,
+  project: {
+    link: 'https://github.com/PostGuitarist/liberty-macos-docs',
+  },
+  chat: {
+    link: 'https://discord.gg/egVb7yPpVf',
+  },
+  docsRepositoryBase: 'https://github.com/PostGuitarist/liberty-macos-docs',
+  footer: {
+    text: 'Made with ❤️ by Cameron and Zaden', 
+  },
+}
+
+export default config
