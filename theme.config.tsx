b5384ae4e82a2c761758a864edcfbfb11f0b0eaa
Zaden Connell <zaden.connell@gmail.com>
2024-01-22 10:59:19 -0500

    Remove unused import from theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
-import { useRouter } from 'next/router'
 
 const config: DocsThemeConfig = {
   logo: <span>LU CS Docs</span>,
