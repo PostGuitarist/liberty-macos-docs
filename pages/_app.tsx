e2407dcc8861006caf2aa596094bbd3f2d3c0acb
Zaden Connell <zaden.connell@gmail.com>
2024-01-23 22:24:39 -0500

    Fix import statement in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
-import themeConfig from '../theme.config.js';
+import themeConfig from '../theme.config';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
